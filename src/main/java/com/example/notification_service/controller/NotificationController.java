2c940e320d1a33603533afc65be816f0d151ef0c
mariya-pshenichnova <m.pshe@yandex.ru>
2024-01-15 21:33:15 +0300

    Рефакторинг

--- src/main/java/com/example/notification_service/controller/NotificationController.java
+++ src/main/java/com/example/notification_service/controller/NotificationController.java
@@ -1,20 +1,26 @@
 package com.example.notification_service.controller;
 
 import com.example.notification_service.model.NotificationRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
-
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class NotificationController {
 
     @PostMapping("/")
     public void sendNotification(@RequestBody NotificationRequest notificationRequest) {
-        Logger log = Logger.getLogger("log");
+        logNotificationRequest(notificationRequest.getText(), notificationRequest.getEmail(), notificationRequest.getPhoneNumber());
+    }
 
-        log.info("Получено сообщение " + notificationRequest.getText() +
-                " на номер телефона " + notificationRequest.getPhoneNumber()
-                + " email " + notificationRequest.getEmail());
+    public void logNotificationRequest(String text, String email, String phoneNumber) {
+        if (email == null && phoneNumber == null) {
+            log.info("Не переданы данные для отправки текста");
+        } else if (email != null && phoneNumber == null) {
+            log.info("Отправь " + text + " на email " + email);
+        } else {
+            log.info("Отправь " + text + " на номер телефона " + phoneNumber);
+        }
     }
 }
--- src/main/java/com/example/notification_service/model/NotificationRequest.java
+++ src/main/java/com/example/notification_service/model/NotificationRequest.java
@@ -1,8 +1,10 @@
 package com.example.notification_service.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NotificationRequest {
     private String text;
     private String email;
